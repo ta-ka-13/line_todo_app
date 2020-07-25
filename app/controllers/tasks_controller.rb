ade8afe0ad290eb0180199fecfa55226a4c9bec7
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-07-26 04:09:02 +0900

    add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,15 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def delete
+    task = Task.find(params[:id])
+    if task.destroy 
+      redirect_to tasks_index_path,
+        notice: "タスクを１件削除しました"
+    else
+      redirect_to tasks_index_path,
+        alert: "タスクの削除に失敗しました"
+    end
+  end
 end
