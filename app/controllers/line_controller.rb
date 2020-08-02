1619d951c842ac196c7312c3be184a0a43b41624
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-08-02 17:16:38 +0900

    Update line_controller.rb

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -10,29 +10,40 @@ require 'line/bot'
     end
 
     events = client.parse_events_from(body)
-    events.each{|event| case event when Line::Bot::Event::Message case event.type when Line::Bot::Event::MessageType::Text
 
+    events.each {|event| 
+      case event when 
+        Line::Bot::Event::Message
+
+
+      case event.type 
+      when 
+        Line::Bot::Event::MessageType::Text
     task = event[‘message’][‘text’]
     begin
       Task.create!(task: task)
 
-      message = {type:'text',text:"タスク『#{task}』を登録しました！"}
+      message = {type:'text',text:"タスク『#{task}』を登録しました！"
+    }
       client.reply_message(event['replyToken'],replyToken'],message)
     rescue
-      message = {type: 'text,text:"タスク『#{task}』の登録に失敗しました。"} 
+      message = {type: 'text,text:"タスク『#{task}』の登録に失敗しました。"
+    } 
       client.reply_message(event['replyToken'],message)
     end
   end
 end
-
-    head :ok
-  end
+}
+  head :ok
+end
 
 
 private
 
   def client
     @client || = Line::Bot::Client.new 
-    { |config| config.channel_secret = ENV["LINE_CHANNEL_SECRET"] config.channel_token = ENV["LINE_CHANNEL_TOKEN"]}
+    { |config| config.channel_secret = ENV["LINE_CHANNEL_SECRET"] 
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
     end
   end
