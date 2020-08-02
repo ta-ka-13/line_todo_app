8af3fdb1f67a8937303f7b2bdfcb31de591d45df
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-08-02 18:36:22 +0900

    Update line_controller.rb

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -47,11 +47,8 @@ private
 
   def client
 
-    @client || = Line::Bot::Client.new
+  @client||= Line::Bot::Client.new
 
-    {|config| 
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"] 
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-    end
+    {|config| config.channel_secret = ENV["LINE_CHANNEL_SECRET"] config.channel_token = ENV["LINE_CHANNEL_TOKEN"]}
   end
+end
