117d39fa318b1e8ba1d8a86b5adbaa1cecdc160b
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-08-15 01:33:22 +0900

    a

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.find(2)
+    @tasks = Task.all
   end
 
   def edit
