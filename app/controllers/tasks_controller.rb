6e814d6c5d3923d7d0aac5d753d0933fa6c5f02c
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-08-19 21:29:02 +0900

    to_s

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   # before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all
+    @tasks = Task.to_s.all
   end
 
   def edit
--- db/migrate/20200606053740_create_tasks.rb
+++ db/migrate/20200606053740_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.string :task
+      t.text :task
 
       t.timestamps
     end
