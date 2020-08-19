4238d7e31cc19237d70728a697ba3aa53945a492
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-08-19 21:10:20 +0900

    migration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
 
 
   def show
-    
+
   end
 
 
--- db/migrate/20200606053740_create_tasks.rb
+++ db/migrate/20200606053740_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.text :task
+      t.string :task
 
       t.timestamps
     end
